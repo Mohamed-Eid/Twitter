3676e7d3782c27a0a4b5fc0c4a6181333c07263d
Mohamed Eid <medoeid50@gmail.com>
2021-12-25 17:27:46 +0200

    wip

---
+++ src/Providers/TwitterServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bluex\Twitter\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TwitterServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+
+    /**
+     * Register the package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
